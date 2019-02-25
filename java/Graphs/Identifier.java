c82bae3f9046ad1aeacdbaadef9ee93907ab9646
NoeFaure <noe.faure@isep.fr>
2019-02-25 14:23:44 -0500

    C7 : Interface

---
+++ java/Graphs/Identifier.java
@@ -0,0 +1,11 @@
+package Graphs;
+
+public class Identifier {
+	
+	// Méthode C3
+	public static void IdentifierIndividus(Individual person_1, Individual person_2)
+	{
+		
+	}
+
+}
--- java/Graphs/Main.java
+++ java/Graphs/Main.java
@@ -2,14 +2,54 @@ package Graphs;
 
 import java.io.IOException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		
+		// ----- Paramètres individuels -----
+		
+		/* Kevin : 
+		 * - /Users/kevin/Documents/Repositories/social-graph-java/ressources/Individus.txt
+		 * - /Users/kevin/Documents/Repositories/social-graph-java/ressources/Relations.txt
+		 * Noé : 
+		 * - /Users/noefaure/Desktop/Homeworks/social-graph-java/ressources/Individus.txt
+		 * - /Users/noefaure/Desktop/Homeworks/social-graph-java/ressources/Relations.txt
+		 */
+		
+		String path_individus = "/Users/noefaure/Desktop/Homeworks/social-graph-java/ressources/Individus.txt";
+		String path_relations = "/Users/noefaure/Desktop/Homeworks/social-graph-java/ressources/Relations.txt";
+		
+		// ----- Paramètres Interfaces -----
+		String choice = "no choice";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while (!choice.equals("a") && !choice.equals("b") && !choice.equals("c") && !choice.equals("d") && !choice.equals("e"))
+		{
+			
+			System.out.println("\n================================");
+			System.out.println("|| --------- Welcome -------- ||");
+			System.out.println("================================");
+			System.out.println("(a) : Créer le réseau social");
+			System.out.println("(b) : Afficher le réseau social");
+			System.out.println("(c) : Jouer à Qui est-ce ?");
+			System.out.println("(d) : Afficher le résultat");
+			System.out.println("(e) : Quitter");
+			System.out.println("\n");
+			System.out.print("Votre choix ?		");
+			
+			choice = br.readLine();
+		}
+		
+		
 		SocialGraph guessWho = new SocialGraph();
 		
-		guessWho.creerReseauSocial("/Users/kevin/Documents/Repositories/social-graph-java/ressources/Individus.txt", "/Users/kevin/Documents/Repositories/social-graph-java/ressources/Relations.txt");
+		guessWho.creerReseauSocial(path_individus, path_relations);
 
 		guessWho.afficherReseauSocial();
+		
 	}
 
 }
